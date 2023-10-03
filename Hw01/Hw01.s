c8d70e95f82f3121793fe8dd24bc92b74a895ec9
BrianCheng-TheLegend <briancheng1229@gmail.com>
2023-10-03 22:43:24 +0800

    2023/10/03

--- Hw01/Hw01.s
+++ Hw01/Hw01.s
@@ -1,7 +1,26 @@
 .data
+# test data 
+test0: .word 0x4141f7cf,0x423645a2
+
+# mask
+mask0: .word 0x7F800000,0x007FFFFF,0xFFFF0000
+mask1: .word 0xFFFF0000,0x0000FFFF
+
+#string
+str: .string "\n"
+
 .text
 main:
-
+    li a7,1
+    lw a0,0(a1)
+    jal ra,fp32_2_bf16
+    ecall
+    
+    j exit
 
 fp32_2_bf16:
     
+    
+exit:
+    li a7,10
+    ecall
